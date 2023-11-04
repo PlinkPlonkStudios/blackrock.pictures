341ba320f4bfdebf9d2a42b65f8e7eb4b4af8cf9
zacmonroe <zmonroe0820@gmail.com>
2023-11-04 10:56:50 -0500

    refactor(ContactForm): move to own file

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 // @index(['./*.ts', './*.tsx'], f => `export * from "${f.path}";`)
 export * from "./AboutCrewMember";
+export * from "./ContactForm";
 export * from "./Footer";
 export * from "./Heading";
 export * from "./NavBar";
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import type { HeadFC, PageProps } from "gatsby";
 import styled from "styled-components";
 
-import { Heading, PageTemplate } from "../components";
+import { ContactForm, Heading, PageTemplate } from "../components";
 import { wideSilhouetteCircle } from "../images";
 
 const MainContent = styled.div`
@@ -82,146 +82,6 @@ const ContactSection = styled.section`
   align-items: center;
 `;
 
-const ContactForm = styled.article`
-  --form-gap: 2em;
-
-  display: grid;
-  gap: var(--form-gap);
-
-  width: min(100%, 40rem);
-
-  margin-block: 30em;
-  padding: 3em;
-
-  background-color: var(--clr-secondary-400);
-  color: var(--clr-fill-400);
-  border-radius: 2em;
-
-  & a {
-    color: inherit;
-    font-weight: bold;
-  }
-
-  & > header {
-    display: grid;
-    gap: var(--form-gap);
-
-    & > h2 {
-      font-weight: bold;
-    }
-  }
-
-  & > form {
-    display: grid;
-    justify-items: stretch;
-    gap: 1em;
-
-    & input[type="email"] {
-      width: 50%;
-
-      @media screen and (max-width: 50rem) {
-        width: 100%;
-      }
-    }
-
-    & label {
-      display: grid;
-      gap: 0.5em;
-
-      & > p {
-        font-weight: bold;
-      }
-    }
-
-    & input,
-    & textarea {
-      padding: 0.5em;
-    }
-
-    & input[type="text"],
-    & textarea {
-      width: 100%;
-    }
-
-    & textarea {
-      resize: vertical;
-      padding-block: 1em;
-    }
-
-    & > footer {
-      display: flex;
-      width: 100%;
-      gap: 1em;
-
-      justify-content: stretch;
-      align-items: stretch;
-
-      & > button {
-        position: relative;
-        isolation: isolate;
-
-        flex: 1;
-        padding: 1rem 2em;
-        margin-top: 0.5em;
-
-        font-weight: bold;
-        text-transform: uppercase;
-        font-size: 2rem;
-
-        background-color: var(--clr-accent-400);
-        border-color: transparent;
-        /* border-radius: 1em; */
-        overflow: none;
-      }
-    }
-  }
-`;
-
-const CF = () => (
-  <ContactForm>
-    <header>
-      <h2>We heard you wanna talk.</h2>
-      <p>
-        Well, we do too. Leave us a message, or email us{" "}
-        <a href="mailto:hello@plinkplonkstudios.com">directly</a>!
-      </p>
-    </header>
-
-    <form>
-      <label htmlFor="email">
-        <p>Your Email</p>
-        <input
-          id="email"
-          type="email"
-          autoComplete="email"
-          placeholder="hello@plinkplonkstudios.com"
-        />
-      </label>
-      <label htmlFor="subject">
-        <p>Subject</p>
-        <input
-          id="subject"
-          type="text"
-          placeholder="Chicago Band interested in collaborating on music videos"
-        />
-      </label>
-
-      <label htmlFor="body">
-        <p>Body</p>
-        <textarea
-          id="body"
-          rows={6}
-          placeholder="We are PlinkPlonk Studios. We are one."
-        />
-      </label>
-
-      <footer>
-        <button type="submit">Send</button>
-      </footer>
-    </form>
-  </ContactForm>
-);
-
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <PageTemplate>
@@ -238,7 +98,7 @@ const IndexPage: React.FC<PageProps> = () => {
         </HeroSection>
 
         <ContactSection>
-          <CF />
+          <ContactForm />
         </ContactSection>
       </MainContent>
     </PageTemplate>
