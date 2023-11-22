5d90dc486c55229c569aaf79888ff6ebb60764d0
zacmonroe <zmonroe0820@gmail.com>
2023-11-22 16:52:05 -0500

    feat(images): add some images

--- src/images/index.ts
+++ src/images/index.ts
@@ -2,8 +2,10 @@
 
 // @index('./*.*', (f, _) => `export { default as ${_.camelCase(f.name)} } from "${f.path}${f.ext}";`)
 export { default as favicon } from "./favicon.png";
+export { default as indigoSquiggle } from "./indigo-squiggle.png";
 export { default as instagramLogo } from "./instagram-logo.png";
 export { default as reelPlaceholder } from "./reel-placeholder.mp4";
+export { default as reelSlice } from "./reel-slice.png";
 export { default as wideSilhouetteCircle } from "./wide-silhouette-circle.png";
 export { default as wideSilhouetteCropped } from "./wide-silhouette-cropped.png";
 export { default as wideSilhouette } from "./wide-silhouette.png";
